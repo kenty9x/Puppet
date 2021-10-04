3d154eb638155023e7e1c5ea08c59ecd982ac194
kenty9x <hoahocqn@gmail.com>
2021-10-04 22:29:06 +0200

    site.pp

--- site.pp
+++ site.pp
@@ -4,9 +4,26 @@ package {'nginx':
     ensure => installed,
 }
 
-file {'/tmp/status.txt':
-    content => 'Nginx installed',
+$packages = ['apache2', 'mysql-server']
+package {$packages:
+    ensure => installed,
+}
+
+$text = "Nginx installed"
+
+file {'/tmp/nginx_status.txt':
+    content => $text,
     mode => '0644',
+}
+
+exec {"Checking apache":
+    command => "/bin/echo apache2 is installed > /tmp/status_apache.txt",
+    onlyif => "/bin/which apache2",
+}
+
+exec {"Checking shell":
+    command => "/bin/echo shell is not installed > /tmp/status_shell.txt",
+    unless => "/bin/which sh",
 }
 
 }
